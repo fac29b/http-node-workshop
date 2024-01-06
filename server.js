f899fd4c7a575d7602fb108b59a6c05beed1e387
sam hockaday <samuel.hockaday@gmail.com>
2024-01-06 17:02:20 +0000

    added middleware

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const server = express();
+const staticHandler = express.static("public");
 
+
+// server.use(logger);
+server.use(staticHandler);
 //Routes
+server.get("/", (request, response, next) => {
+    console.log(request.method + " " + request.url);
+    next();
+});
 
+server.get("/", (request, response) => {
+    response.send(`<h1>Hello</h1>`);
+});
 server.get("/", (request, response) => {
     const year = new Date().getFullYear();
     response.send(`
@@ -11,6 +22,7 @@ server.get("/", (request, response) => {
       <head>
         <meta charset="utf-8">
         <title>Home</title>
+        <link rel="stylesheet" href="/style.css">
       </head>
       <body>
         <h1>Hello, it's ${year}</h1>
@@ -25,6 +37,9 @@ server.get("/search", (request, response) => {
     const keyword = request.query.keyword;
     response.send(`<p>You searched for ${keyword}</p>`)
 })
+server.use((request, response) => {
+    response.status(404).send("<h1> Not Found</h1>");
+});
 
 module.exports = server;
 
